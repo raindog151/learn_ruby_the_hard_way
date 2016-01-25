6e1bb68e26df955b498b96c7a6ebb95b21b0bb50
Daniel Nagy <daniel.nagy@target.com>
2016-01-24 22:27:48 -0600

    ex40 - 41

---
+++ ex40.rb
@@ -0,0 +1,18 @@
+class Song
+
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line}
+  end
+end
+
+happy_bday = Song.new(["Happy birthday to you", "I don't want to get sued", "So I'll stop there."])
+
+bulls_on_parade = Song.new(["They rally roung the family", "With a pocket full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
